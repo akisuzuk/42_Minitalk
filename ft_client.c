a527c97af427aaeee2e2a9f62227c29424af8d3a
akisuzuk <122352761+akisuzuk@users.noreply.github.com>
2023-07-30 20:24:06 +0900

    test

--- ft_client.c
+++ ft_client.c
@@ -6,15 +6,14 @@
 /*   By: akisuzuk <akisuzuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/16 10:54:46 by akisuzuk          #+#    #+#             */
-/*   Updated: 2023/07/16 18:44:44 by akisuzuk         ###   ########.fr       */
+/*   Updated: 2023/07/30 20:11:49 by akisuzuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// file:///Users/akisuzuk/Downloads/645763cf71dca0d205219559.pdf
+// https://onedrive.live.com/?cid=A5113E935F3D0D56&id=A5113E935F3D0D56%2132182&parId=A5113E935F3D0D56%2132181&o=OneUp
 // https://onedrive.live.com/?authkey=%21ABrqrVGxVxFhHMU&id=518C1E08C43073F5%211605&cid=518C1E08C43073F5&parId=root&parQt=sharedby&parCid=A5113E935F3D0D56&o=OneUp
 // https://qiita.com/keitean/items/c641b3e158e01dacb7f6
 // https://en-junior.com/sigaction-signal/
-
 
 // signalとsigactionを使う課題だと理解
 // シグナルハンドラという、プログラムに対してシグナルが送られた時に実行される関数を作っていく(登録する、と言う
@@ -37,9 +36,11 @@
 // 今回使えるシグナルはSIGUSR1とSIGUSR2のみ
 
 // 今回の課題では、サーバー側で受け取った文字を表示するということを行う
-// 流れとしてはserverをバックグラウンドで起動し、その後clientを起動する
-// clientは、kill関数を使ってサーバーに文字を送る
-// これ、ターミナルを2つ開いて、それぞれでserverとclientを起動することで実現できるってこと？
+// 流れとしてはserver.cをバックグラウンドで起動し、その後client.cを起動する
+// client.cは、kill関数を使ってサーバーに文字を送る
+// これ、ターミナルを2つ開いて、それぞれでserver.cとclient.cを起動することで実現できるってこと？
+
+
 
 
 #include "ft_minitalk.h"
