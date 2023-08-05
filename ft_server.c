287820e424f4f7b5ed9afde2f35fce6b74d993bf
akisuzuk <122352761+akisuzuk@users.noreply.github.com>
2023-08-06 00:45:35 +0900

    begin writing server

--- ft_server.c
+++ ft_server.c
@@ -6,12 +6,20 @@
 /*   By: akisuzuk <akisuzuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/16 10:54:50 by akisuzuk          #+#    #+#             */
-/*   Updated: 2023/07/16 13:42:40 by akisuzuk         ###   ########.fr       */
+/*   Updated: 2023/08/06 00:45:10 by akisuzuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main()
+#include "ft_minitalk.h"
+
+int	main(void)
 {
+	struct sigaction	act1;
+	struct sigaction	act2;
+
+	printf("not so bad minitalk server pid=%d\n", getpid());
+	//KOKOMADE SUSUNDA
+
 	while (1)
 		return (0);
 }
