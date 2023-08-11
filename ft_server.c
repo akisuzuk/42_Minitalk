b99760470d0a6e9428c0a6a1720150afdb97d5b6
akisuzuk <122352761+akisuzuk@users.noreply.github.com>
2023-08-12 00:09:19 +0900

    20230811

--- ft_server.c
+++ ft_server.c
@@ -6,7 +6,7 @@
 /*   By: akisuzuk <akisuzuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/16 10:54:50 by akisuzuk          #+#    #+#             */
-/*   Updated: 2023/08/06 00:45:10 by akisuzuk         ###   ########.fr       */
+/*   Updated: 2023/08/12 00:00:58 by akisuzuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,15 @@ int	main(void)
 	struct sigaction	act2;
 
 	printf("not so bad minitalk server pid=%d\n", getpid());
-	//KOKOMADE SUSUNDA
+	// 構造体の初期化
+	ft_memset(&act1, 0, sizeof(sigaction));
+	ft_memset(&act2, 0, sizeof(sigaction));
+	act1.sa_sigaction = signal_handler;
+	act2.sa_sigaction = signal_handler;
+	// フラグは0でもOKらしいが、SA_SIGINFOを指定する
+	// (両者の違い分かってないけど)SA_SIGINFOを指定するとsa_sigactionが呼ばれる
+	// SA_SIGINFOを指定しないとsa_handlerが呼ばれる
+	act1.sa_flags = SA_SIGINFO;
 
 	while (1)
 		return (0);
